f0ac5ad9a2c968455bf50a4f27af3a02ca3b743f
ktapia576 <ktapia576@hotmail.com>
2021-01-14 01:54:22 -0500

    Update

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+class App extends React.Component {
+    componentDidMount() {
+        axios.get('http://localhost:3000/users')
+            .then(response => {
+                console.log(response.data);
+            })
+    }
+    render() {
+        return (
+            <h1> Welcome to REACT!!</h1>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
